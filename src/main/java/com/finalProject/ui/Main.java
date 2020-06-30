e0229408fcb9bc5ff821a6aecc2d50f96db725b6
jakubzzak <zak.jakub67@gmail.com>
2020-06-30 17:29:14 +0200

    some mor dosc

--- src/main/java/com/finalProject/ui/Main.java
+++ src/main/java/com/finalProject/ui/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
+//import java.util.Properties;
 
 
 /**
@@ -18,28 +18,81 @@ import java.util.Properties;
  Stores all values needed to be accessed globally.
  */
 public class Main extends Application {
+    /**
+     * Global instance of primary stage.
+     */
     public static Stage ps; // window size -> global access
+    /**
+     * Global instance of scene.
+     */
     public static Scene sc; // showing components -> global access
+    /**
+     * Global instance of user.
+     */
     private static User user;
-    private static Properties props;
+//    private static Properties props;
+    /**
+     * Global instance of chosen level.
+     */
     private static Level currentLevel;
+    /**
+     * Global instance of list of all levels.
+     */
     private static List<Level> levels;
+    /**
+     * Global instance of screen handler.
+     */
     private static final ScreensController screenContainer = new ScreensController();
+    /**
+     * Global instance of initial height.
+     */
     public static double INIT_HEIGHT = 250; // TODO: move to config
+    /**
+     * Global instance of initial width.
+     */
     public static double INIT_WIDTH = 300;
+    /**
+     * Global instance of current height.
+     */
     public static double HEIGHT = INIT_HEIGHT; // TODO: move to config
+    /**
+     * Global instance of current width.
+     */
     public static double WIDTH = INIT_WIDTH;
 
+    /**
+     * Prop for welcome screen source.
+     */
     public static String welcomeScreenID = "welcome";
+    /**
+     * Prop for welcome screen source.
+     */
     public static String welcomeScreenSOURCE = "/views/welcomeScreen.fxml";
+    /**
+     * Prop for game screen source.
+     */
     public static String gameScreenID = "game";
+    /**
+     * Prop for game screen source.
+     */
     public static String gameScreenSOURCE = "/views/gameScreen.fxml";
+    /**
+     * Prop for level screen source.
+     */
     public static String lvlScreenID = "level";
+    /**
+     * Prop for level screen source.
+     */
     public static String lvlScreenSOURCE = "/views/levelScreen.fxml";
 //    private static String screen3ID = "help";
 //    private static String screen3File = "/views/helpScreen.fxml";
 
 
+    /**
+     * Kick-start of the application.
+     * @param primaryStage
+     * Stage to visualize app on the screen.
+     */
     @Override
     public void start(Stage primaryStage) {
         user = new User();
@@ -92,20 +145,48 @@ public class Main extends Application {
         }
     }
 
+    /**
+     * Get the instance of the user.
+     * @return
+     * Object for handling user.
+     */
     public static User getUser() { return user; }
-    public static ScreensController getScreenContainer() { return screenContainer; }
-    public static List<Level> getLevels() { return levels; }
-    public static Stage getPrimaryStage() { return ps; }
-    public static Level getCurrentLevel() { return currentLevel; }
-    public double getHeight() { return INIT_HEIGHT; }
-    public double getWidth() { return INIT_WIDTH; }
-    public static Properties getProps() {
-        return props;
-    }
 
+    /**
+     * Get the instance of the screen controller.
+     * @return
+     * Instance of the screen controller.
+     */
+    public static ScreensController getScreenContainer() { return screenContainer; }
+
+    /**
+     * Get the list of levels presented in the src/levels/ folder.
+     * @return
+     * List of levels.
+     */
+    public static List<Level> getLevels() { return levels; }
+
+    /**
+     * Gets the current level.
+     * @return
+     * Currently played level.
+     */
+    public static Level getCurrentLevel() { return currentLevel; }
+//    public double getHeight() { return INIT_HEIGHT; }
+//    public double getWidth() { return INIT_WIDTH; }
+//    public static Properties getProps() {
+//        return props;
+//    }
+//
+
+    /**
+     * Sets the current level.
+     * @param currentLevel
+     * Currently played level.
+     */
     public static void setCurrentLevel(Level currentLevel) { Main.currentLevel = currentLevel; }
-    public void setHeight(double newSize) { INIT_HEIGHT = newSize; }
-    public void setWidth(double newSize) { INIT_WIDTH = newSize; }
+//    public void setHeight(double newSize) { INIT_HEIGHT = newSize; }
+//    public void setWidth(double newSize) { INIT_WIDTH = newSize; }
 
     /**
      Goes through all files in directory 'resources/levels'.
